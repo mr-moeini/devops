93d37f9f6d17dd3d9f3f48ba0c1a56ae569466e4
mr-moeini <arta.moeini@gmail.com>
2024-11-01 00:28:03 +0330

    Update k8s_install.sh

--- installation/scripts/k8s_install.sh
+++ installation/scripts/k8s_install.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# Update the system packages
+# Update package
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl
 
-# Add Aliyun  repository
-sudo curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
-EOF'
+# Add the repository for Ubuntu 22.04
+sudo curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-jammy main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-# Update the package list
-sudo apt-get update
+# Update the package list with the new repository
+sudo apt update
 
 # Install Kubernetes components
 sudo apt install -y kubelet kubeadm kubectl
 
-# Hold kubernetes to prevent automatic updates
+# Hold packages to prevent automatic updates
 sudo apt-mark hold kubelet kubeadm kubectl
 
 # Enable and start kubelet service
 sudo systemctl enable kubelet && sudo systemctl start kubelet
 
-echo "Kubernetes has been installed"
+echo "Kubernetes have been installed successfully!"
+
